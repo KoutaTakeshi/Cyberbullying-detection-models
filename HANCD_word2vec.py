217e78163ad4edeceee2af710c90c3747f37d8d9
KoutaTakeshi <353079908@qq.com>
2024-01-12 11:43:29 +0800

    style: Updated code formatting

--- HANCD_word2vec.py
+++ HANCD_word2vec.py
@@ -23,7 +23,7 @@ np.random.seed(0)
 MAX_SENT_LENGTH = 20###number of words in a sentence
 MAX_NB_WORDS = 20000
 POST_DIM = 400
-INFO_DIM=30
+INFO_DIM = 30
 VALIDATION_SPLIT = 0.2
 
 ##slice tensor function in keras
@@ -45,10 +45,10 @@ def crop(dimension, start, end):
     return Lambda(func)
 
 def myFunc(x):
-  if "empety" in x:
-    return False
-  else:
-    return True
+    if "empety" in x:
+        return False
+    else:
+        return True
 
 def clean_str(string):
     """
@@ -116,20 +116,21 @@ class AttLayer(Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[-1])
 
+# Load data
 with open('instagram.pickle', 'rb') as handle:
     dictionary = pickle.load(handle)
-texts=dictionary['text']
+texts = dictionary['text']
 #texts=[text.encode('ascii') for text in texts]
-comments=dictionary['comments']
-timeInfo=dictionary['time']
-postInfo=dictionary['post']
-labels=dictionary['labels']
+comments = dictionary['comments']
+timeInfo = dictionary['time']
+postInfo = dictionary['post']
+labels = dictionary['labels']
 
-b = np.zeros([len(timeInfo),len(max(timeInfo,key = lambda x: len(x)))])
-for i,j in enumerate(timeInfo):
+b = np.zeros([len(timeInfo), len(max(timeInfo, key = lambda x: len(x)))])
+for i, j in enumerate(timeInfo):
     b[i][0:len(j)] = j
-timeInfo=b
-time_size=len(np.unique(timeInfo))
+timeInfo = b
+time_size = len(np.unique(timeInfo))
 MAX_SENTS = len(timeInfo[0])####number of sentences
 
 postInfo=np.array(postInfo)
@@ -151,7 +152,7 @@ for i, sentences in enumerate(comments):
 
 word_index = tokenizer.word_index
 print('Total %s unique tokens.' % len(word_index))
-single_label=np.asarray(labels)
+single_label = np.asarray(labels)
 labels = to_categorical(np.asarray(labels))
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
@@ -183,33 +184,33 @@ for j in range(10):
     data1 = data[indices]
     print(data1.shape)
     labels1 = labels[indices]
-    single_label1=single_label[indices]
-    timeInfo1=timeInfo[indices]
-    print (timeInfo1.shape)
-    timeInfo1=timeInfo1.reshape((2218,MAX_SENTS,1))
-    print (timeInfo1.shape)
-    data1=np.dstack((data1, timeInfo1))
-    postInfo1=postInfo[indices]
-    print (postInfo1.shape)
+    single_label1 = single_label[indices]
+    timeInfo1 = timeInfo[indices]
+    print(timeInfo1.shape)
+    timeInfo1 = timeInfo1.reshape((2218, MAX_SENTS, 1))
+    print(timeInfo1.shape)
+    data1 = np.dstack((data1, timeInfo1))
+    postInfo1 = postInfo[indices]
+    print(postInfo1.shape)
     nb_validation_samples = int(VALIDATION_SPLIT * data1.shape[0])
-    zeros=np.zeros(2218)
-    zeros=zeros.reshape((2218,1,1))
+    zeros = np.zeros(2218)
+    zeros = zeros.reshape((2218, 1, 1))
 
     x_train = data1[:-nb_validation_samples]
     y_train = labels1[:-nb_validation_samples]
-    zeros_train=zeros[:-nb_validation_samples]
-    time_train=timeInfo1[:-nb_validation_samples]
-    post_train=postInfo1[:-nb_validation_samples]
+    zeros_train = zeros[:-nb_validation_samples]
+    time_train = timeInfo1[:-nb_validation_samples]
+    post_train = postInfo1[:-nb_validation_samples]
     x_val = data1[-nb_validation_samples:]
     y_val = labels1[-nb_validation_samples:]
-    zeros_test=zeros[-nb_validation_samples:]
-    time_test=timeInfo1[-nb_validation_samples:]
-    post_test=postInfo1[-nb_validation_samples:]
-    y_single=single_label1[-nb_validation_samples:]
+    zeros_test = zeros[-nb_validation_samples:]
+    time_test = timeInfo1[-nb_validation_samples:]
+    post_test = postInfo1[-nb_validation_samples:]
+    y_single = single_label1[-nb_validation_samples:]
 
     print('Number of positive and negative posts in training and test set')
-    print (y_train.sum(axis=0))
-    print (y_val.sum(axis=0))
+    print(y_train.sum(axis=0))
+    print(y_val.sum(axis=0))
 
     # building Hierachical Attention network
 
@@ -220,60 +221,59 @@ for j in range(10):
                                 trainable=True,
                                 mask_zero=True)
 
-    all_input = Input(shape=(MAX_SENT_LENGTH+1,))
-    sentence_input=crop(1, 0, MAX_SENT_LENGTH)(all_input)##slice
-    time_input=crop(1, MAX_SENT_LENGTH, MAX_SENT_LENGTH+1)(all_input)##slice
+    all_input = Input(shape=(MAX_SENT_LENGTH+1, ))
+    sentence_input = crop(1, 0, MAX_SENT_LENGTH)(all_input)##slice
+    time_input = crop(1, MAX_SENT_LENGTH, MAX_SENT_LENGTH + 1)(all_input)##slice
     embedded_sequences = embedding_layer(sentence_input)
     l_lstm = Bidirectional(GRU(100, return_sequences=True))(embedded_sequences)
     l_att = AttLayer(100)(l_lstm)####(?,200)
     #time_embedding=Dense(TIME_DIM,activation='sigmoid')(time_input)
-    merged_output=Concatenate()([l_att,time_input])###text+time information
+    merged_output=Concatenate()([l_att, time_input])###text+time information
     sentEncoder = Model(all_input, merged_output)
 
-    review_input = Input(shape=(MAX_SENTS, MAX_SENT_LENGTH+1))
+    review_input = Input(shape=(MAX_SENTS, MAX_SENT_LENGTH + 1))
     review_encoder = TimeDistributed(sentEncoder)(review_input)
     l_lstm_sent = Bidirectional(GRU(100, return_sequences=True))(review_encoder)
     #pred_time=Dense(1,activation='relu')(l_lstm_sent)
-    fully_sent=Dense(1,use_bias=False)(l_lstm_sent)
-    norm_fullysent=BatchNormalization()(fully_sent)
-    pred_time=Activation(activation='linear')(norm_fullysent)
+    fully_sent = Dense(1, use_bias=False)(l_lstm_sent)
+    norm_fullysent = BatchNormalization()(fully_sent)
+    pred_time = Activation(activation='linear')(norm_fullysent)
 
-    zero_input=Input(shape=(1,1))
-    shift_predtime=Concatenate(axis=1)([zero_input,pred_time])
-    shift_predtime=crop(1, 0, MAX_SENTS)(shift_predtime)
+    zero_input = Input(shape=(1, 1))
+    shift_predtime = Concatenate(axis=1)([zero_input, pred_time])
+    shift_predtime = crop(1, 0, MAX_SENTS)(shift_predtime)
     l_att_sent = AttLayer(100)(l_lstm_sent)
 
     ###embed the #likes, shares
-    post_input=Input(shape=(4,))
+    post_input = Input(shape=(4, ))
     #post_embedding = Dense(INFO_DIM, activation='sigmoid')(post_input)
-    fully_post=Dense(INFO_DIM,use_bias=False)(post_input)
-    norm_fullypost=BatchNormalization()(fully_post)
-    post_embedding=Activation(activation='relu')(norm_fullypost)
-    x = concatenate([l_att_sent,post_embedding])###merge the document level vectro with the additional embedded features such as #likes
-    fully_review=Dense(2,use_bias=False)(x)
-    norm_fullyreview=BatchNormalization()(fully_review)
-    preds=Activation(activation='softmax')(norm_fullyreview)
+    fully_post = Dense(INFO_DIM, use_bias=False)(post_input)
+    norm_fullypost = BatchNormalization()(fully_post)
+    post_embedding = Activation(activation='relu')(norm_fullypost)
+    x = concatenate([l_att_sent, post_embedding])###merge the document level vectro with the additional embedded features such as #likes
+    fully_review = Dense(2, use_bias=False)(x)
+    norm_fullyreview = BatchNormalization()(fully_review)
+    preds = Activation(activation='softmax')(norm_fullyreview)
 
     rmsprop = optimizers.adam(lr=0.001)
-    model = Model(inputs=[review_input,post_input,zero_input], outputs=[preds,shift_predtime])
+    model = Model(inputs=[review_input, post_input, zero_input], outputs=[preds, shift_predtime])
     #print(model.summary())
-    model.compile(loss=['binary_crossentropy','mse'],loss_weights=[1,0.00002],
-                  optimizer=rmsprop)
+    model.compile(loss=['binary_crossentropy', 'mse'], loss_weights=[1, 0.00002], optimizer=rmsprop)
     #filepath = "weights/weights-improvement-{epoch:02d}-{loss:.2f}.hdf5"
     #checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     #callbacks_list = [EarlyStopping(monitor='loss', patience=1,mode='min'),checkpoint]
 
     print("model fitting - Hierachical attention network for cyberbullying detection")
 
-    model.fit([x_train,post_train,zeros_train], [y_train,time_train], batch_size=16,
-              epochs=1,verbose=1)
-    yp = model.predict([x_val,post_test,zeros_test], verbose=1)
-    ypreds=yp[0]
+    model.fit([x_train, post_train, zeros_train], [y_train, time_train], batch_size=16,
+              epochs=1, verbose=1)
+    yp = model.predict([x_val, post_test, zeros_test], verbose=1)
+    ypreds = yp[0]
     ypreds = np.argmax(ypreds, axis=1)
     #print y_single
     #print ypred
-    f1=precision_recall_fscore_support(y_single, ypreds)
-    auc=roc_auc_score(y_single, ypreds)
+    f1 = precision_recall_fscore_support(y_single, ypreds)
+    auc = roc_auc_score(y_single, ypreds)
     print(f1)
     print(auc)
     HAN_AUC.append(auc)
@@ -282,10 +282,10 @@ for j in range(10):
     HAN_pre.append(f1[0][1])
 
     #for t-sne visualization
-    if j==0:
-        a=model.layers
-        get_representations_test = K.function([model.layers[0].input,model.layers[1].input,model.layers[12].input], [model.layers[6].output])
-        representations_test = get_representations_test([x_val,post_test,zeros_test])[0]
+    if j == 0:
+        a = model.layers
+        get_representations_test = K.function([model.layers[0].input, model.layers[1].input, model.layers[12].input], [model.layers[6].output])
+        representations_test = get_representations_test([x_val, post_test, zeros_test])[0]
         representation_dict = {
             'representations': representations_test,
             'labels': y_single
@@ -295,12 +295,12 @@ for j in range(10):
             pickle.dump(representation_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
     K.clear_session()
 
-print (HAN_AUC)
-print (HAN_f1)
-print (HAN_pre)
-print (HAN_reca)
-print ("AUC",np.mean(HAN_AUC), np.std(HAN_AUC))
-print ("f1", np.mean(HAN_f1), np.std(HAN_f1))
-print ("precision",np.mean(HAN_pre), np.std(HAN_pre))
-print ("recall", np.mean(HAN_reca), np.std(HAN_reca))
+print(HAN_AUC)
+print(HAN_f1)
+print(HAN_pre)
+print(HAN_reca)
+print("AUC",np.mean(HAN_AUC), np.std(HAN_AUC))
+print("f1", np.mean(HAN_f1), np.std(HAN_f1))
+print("precision",np.mean(HAN_pre), np.std(HAN_pre))
+print("recall", np.mean(HAN_reca), np.std(HAN_reca))
 
